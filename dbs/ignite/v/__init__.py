78021a3e114c001bf6ae75d6421c007a01ba4584
zstan <stanilovsky@gmail.com>
2020-12-03 16:20:39 +0300

    fix

--- dbs/ignite/v/__init__.py
+++ dbs/ignite/v/__init__.py
@@ -1,9 +1,7 @@
-from pyignite import Client
-import sqlite3
-import pyignite
+import jaydebeapi
 
-client = Client()
-client.connect('127.0.0.1', 10800)
+conn = jaydebeapi.connect("org.apache.ignite.IgniteJdbcThinDriver", "jdbc:ignite:thin://127.0.0.1?useExperimentalQueryEngine=true", ["", ""],
+    "/home/zstan/work/repo/apache-ignite/modules/core/target/ignite-core-2.10.0-SNAPSHOT.jar")
 
 exclude = ['GRANT', 'CREATE SCHEMA', 'CREATE ROLE', 'PRIVILEGES ON', 'REFERENCES ON', 'CREATE VIEW', 'CREATE ROLE']
 
@@ -24,18 +22,12 @@ def run_test(test):
             if (sql.find("CREATE TABLE") != -1 and sql.find('PRIMARY KEY') == -1):
                 pos1 = sql.rfind(")")
                 sql = sql[:pos1-1] + " PRIMARY KEY, CC INTEGER " + sql[pos1-1:]
-            print (sql)
-            client.sql(sql)
-        print('complete')
-
-        # conn.commit()
-        #conn.close()
-    except pyignite.exceptions.SQLError as e:
+            print ("start execute: " + sql)
+            with conn.cursor() as curs:
+                curs.execute(sql)
+        print("complete: " + sql)
+    except Exception as e:
+        print (e)
         error = e
-    except pyignite.exceptions.ParseError as e1:
-        print (e1)
-        client.connect('127.0.0.1', 10800)
-    except Exception as e0:
-        print ("!!!: " + e0)
 
     return error
