25a0d51a6ced0de0c63a54b9b06fd080e7008cff
zstan <stanilovsky@gmail.com>
2021-08-03 20:54:35 +0300

    2.11 ver

--- dbs/ignite/v/__init__.py
+++ dbs/ignite/v/__init__.py
@@ -1,7 +1,7 @@
 import jaydebeapi
 
 conn = jaydebeapi.connect("org.apache.ignite.IgniteJdbcThinDriver", "jdbc:ignite:thin://127.0.0.1?useExperimentalQueryEngine=true", ["", ""],
-    "/home/zstan/work/repo/apache-ignite/modules/core/target/ignite-core-2.10.0-SNAPSHOT.jar")
+    "/home/zstan/work/repo/apache-ignite/modules/core/target/ignite-core-2.11.0-SNAPSHOT.jar")
 
 exclude = ['GRANT', 'CREATE SCHEMA', 'CREATE ROLE', 'PRIVILEGES ON', 'REFERENCES ON', 'CREATE VIEW', 'CREATE ROLE']
 
