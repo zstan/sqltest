377e1e65a7dee7747480d59fd62e48c281d08425
zstan <stanilovsky@gmail.com>
2022-03-24 11:35:39 +0300

    jdbc new ver

--- dbs/ignite/v/__init__.py
+++ dbs/ignite/v/__init__.py
@@ -1,7 +1,7 @@
 import jaydebeapi
 
 conn = jaydebeapi.connect("org.apache.ignite.IgniteJdbcThinDriver", "jdbc:ignite:thin://127.0.0.1?useExperimentalQueryEngine=true", ["", ""],
-    "/home/zstan/work/repo/apache-ignite/modules/core/target/ignite-core-2.11.0-SNAPSHOT.jar")
+    "/home/zstan/work/repo/apache-ignite/modules/core/target/ignite-core-2.13.0-SNAPSHOT.jar")
 
 exclude = ['ON UPDATE', 'FOREIGN KEY', 'GRANT', 'CREATE SCHEMA', 'CREATE ROLE', 'PRIVILEGES ON', 'REFERENCES ON', 'CREATE VIEW', 'CREATE ROLE']
 
